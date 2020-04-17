bc0d3788ea12095804a19a41b405628a994417d4
nkoerb <47297576+nkoerb@users.noreply.github.com>
2020-04-17 16:37:30 +0200

    reworked data loading and processing

---
+++ dl/dl_datagenerator.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 09:47:31 2019
+
+@author: Koerber
+"""
+
+
+
+import cv2
+import numpy as np
+import random
+import threading
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from tensorflow.python.keras.utils.data_utils import Sequence
+import dl.dl_augment as dl_augment
+import dl.dl_utils as dl_utils
+from utils.Contour import LoadLabel
+
+
+
+class TrainingDataGenerator_inMemory(Sequence):
+    def __init__(self, parent):
+        self.parent = parent
+        self.batch_size = parent.batch_size
+        self.lock = threading.Lock() 
+        images,labels = parent.dataloader.loadTrainingDataSet()
+        self.images = images
+        self.labels = labels
+        self.indices = list(range(0,images.shape[0]))
+        self.numImages = len(self.indices)
+        random.shuffle(self.indices)
+    
+    def __len__(self):
+        return int(np.ceil(self.numImages / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        with self.lock:
+            batch_start = idx * self.batch_size
+            batch_end = min(batch_start + self.batch_size, self.numImages)
+
+            batch_images = self.images[self.indices[batch_start:batch_end]]
+            batch_masks = self.labels[self.indices[batch_start:batch_end]]
+
+            img, mask = dl_augment.augment(batch_images,batch_masks)         
+            img = self.parent.dataloader.preprocessImage(img)     
+            mask = self.parent.dataloader.preprocessLabel(mask)
+            return img, mask
+        
+    def on_epoch_end(self):
+        random.shuffle(self.indices)
+
+
+class TrainingDataGenerator_fromDisk(Sequence):
+    def __init__(self, parent):
+        self.parent = parent
+        self.batch_size = parent.batch_size
+        self.lock = threading.Lock()
+        self.images = self.parent.dataloader.ImagesPaths
+        self.labels = self.parent.dataloader.LabelsPaths
+        self.channels = 1 if parent.MonoChrome() is True else 3
+        self.numClasses = parent.NumClasses()
+        self.scalefactor = parent.ImageScaleFactor
+        self.indices = list(range(0,len(self.images)))
+        self.numImages = len(self.images)
+        random.shuffle(self.indices)
+
+    def __len__(self):
+        return int(np.ceil(self.numImages / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        with self.lock:
+            batch_images = []
+            batch_masks = []
+            batch_start = idx * self.batch_size
+            batch_end = min(batch_start + self.batch_size, self.numImages)
+            for i in range(batch_start, batch_end):
+                
+                train_img, train_mask = self.parent.dataloader.createImageLabelPair(i)
+
+                batch_images.append(train_img)
+                batch_masks.append(train_mask)
+
+            img, mask = dl_augment.augment(batch_images, batch_masks)
+
+            img = self.parent.dataloader.preprocessImage(img)     
+            mask = self.parent.dataloader.preprocessLabel(mask)
+            return img, mask
+        
+    def on_epoch_end(self):
+        random.shuffle(self.indices)
+    
+    
+
---
+++ dl/dl_imageloader.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 14:56:23 2020
+
+@author: Koerber
+"""
+
+import os
+import glob
+import cv2
+import dl.dl_utils as dl_utils
+from utils.Contour import LoadLabel
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+
+class ImageLoader():
+    def __init__(self, parent):
+        self.parent = parent
+        self.TrainingImagePath = None
+        self.LabelImagePath = None
+        
+        self.ImagesPaths = []
+        self.LabelsPaths = []
+
+        
+    def readImage(self, path):
+        image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+
+        # should we normalize over the dataset?
+        # pro: better for images from the same source, i.e. images that do not contain any target will lead to background that gets amplified by single image normalization
+        # con: single image normalization is better suited for different sources, like 8-bit and 16-bit and color images combined
+        norm_image = np.zeros(image.shape)
+        norm_image = cv2.normalize(image,norm_image, 0, 255, cv2.NORM_MINMAX)
+        image = norm_image.astype('uint8')
+
+        if len(image.shape) == 4:
+            image = cv2.cvtColor(image, cv2.CV_BGRA2BGR )
+        if len(image.shape) == 2:
+            if not self.parent.MonoChrome():
+                image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR )
+            else:
+                image = image[..., np.newaxis]
+        elif len(image.shape) == 3:
+            if self.parent.MonoChrome() and image.shape[2] > 1:
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY )
+                # atm I dont know if bgr2gray squeezes
+                if len(image.shape) == 2:
+                    image = image[..., np.newaxis]    
+        return image
+    
+    
+    def initTrainingDataset(self, imagepath, labelpath):         
+        images = glob.glob(os.path.join(imagepath,'*.*'))
+        labels = glob.glob(os.path.join(labelpath,'*.*'))
+          
+        # to do: check if images in one of supported image formats
+        labels = [x for x in labels if x.endswith(".npy") or x.endswith(".npz")]
+        
+        image_names = [os.path.splitext(os.path.basename(each))[0] for each in images]
+        label_names = [os.path.splitext(os.path.basename(each))[0] for each in labels]
+        
+        intersection = list(set(image_names).intersection(label_names))
+        if intersection == list():
+            return None, None
+        
+        images = [each for each in images if os.path.splitext(os.path.basename(each))[0] in intersection]
+        labels = [each for each in labels if os.path.splitext(os.path.basename(each))[0] in intersection]
+        images.sort()
+        labels.sort()
+        
+        self.ImagesPaths = images
+        self.LabelsPaths = labels
+
+    def initialized(self):
+        return True if len(self.ImagesPaths) * len(self.LabelsPaths) > 0 else False
+
+    def preprocessImage(self, image):
+        return image.astype('float')/255.
+        
+    def preprocessLabel(self,mask):
+        if self.parent.NumClasses() > 2:
+            if self.parent.useWeightedDistanceMap:                 
+                label = mask[...,0]
+                weights = mask[...,1]
+                label = to_categorical(label, num_classes=self.numClasses)
+                mask = np.concatenate((label, weights[...,np.newaxis]), axis = 3) 
+            else:
+                mask = to_categorical(mask, num_classes=self.numClasses)
+        else:
+            mask = mask.astype('float')
+        return mask
+    
+    
+    def removeUnlabelledData(self, image, label):
+        # remove unlabelled data
+        _, thresh = cv2.threshold(label, 0, 255,  cv2.THRESH_BINARY)
+        image = cv2.bitwise_and(image,image, mask=thresh)
+            
+        # remove background
+        label[np.where(label == 255)] = 0
+        return image, label
+
+
+    def addWeightMap(self, label):
+        if self.parent.useWeightedDistanceMap:
+            weights = dl_utils.createWeightedBorderMapFromLabel(label[np.newaxis,...])
+            label = np.concatenate((label, weights[0,...]), axis = 2)
+        return label
+            
+    def createImageLabelPair(self, index, width=None, height=None):
+
+        channels = 1 if self.parent.MonoChrome() is True else 3
+        train_img = self.readImage(self.ImagesPaths[index])
+        
+        if not width or not height:
+            width = int(train_img.shape[1]*self.parent.ImageScaleFactor)
+            height= int(train_img.shape[0]*self.parent.ImageScaleFactor)
+                
+        train_mask = LoadLabel(self.LabelsPaths[index], train_img.shape[0], train_img.shape[1])
+            
+        train_img =  cv2.resize(train_img, (width, height))
+        train_mask = cv2.resize(train_mask, (width, height), interpolation = cv2.INTER_NEAREST )
+
+        train_img, train_mask = self.removeUnlabelledData(train_img, train_mask) 
+
+        train_mask = train_mask.reshape(height, width, 1)
+        train_img = train_img.reshape(height, width, channels)
+            
+        train_mask = self.addWeightMap(train_mask)
+        
+        return train_img, train_mask
+
+    
+    def loadTrainingDataSet(self):
+        if not self.initialized():
+            return
+       
+        images = self.ImagesPaths
+        test = cv2.imread(images[0], cv2.IMREAD_UNCHANGED)
+        width = int(test.shape[1]*self.parent.ImageScaleFactor)
+        height = int(test.shape[0]*self.parent.ImageScaleFactor)
+        
+        
+        im_channels = 1 if self.parent.MonoChrome() is True else 3
+        lb_channels = 2 if self.parent.useWeightedDistanceMap else 1
+        # now: all images are resized to the size of the first image 
+        # Alternatively: all images could be padded to the largest image size
+        img = np.zeros((len(images), height, width, im_channels)).astype('uint8')
+        mask = np.zeros((len(images), height, width, lb_channels)).astype('uint8')
+        
+        
+        for i in range (len(images)):   
+            # to do: process with workers a la: with concurrent.futures.ThreadPoolExecutor(...) as executor:
+            train_img, train_mask = self.createImageLabelPair(i, width=width, height=height)
+              
+            img[i] = train_img
+            mask[i] = train_mask
+            
+        return img, mask
+    
+    
+    
+    
+
