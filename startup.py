e8e63cb39d485e888f03c3330f4db1b4272137f4
nkoerb <47297576+nkoerb@users.noreply.github.com>
2021-06-28 17:10:58 +0200

    added rel path for package

--- startup.py
+++ startup.py
@@ -3,30 +3,38 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from ui.style import setStyle
 
-if __name__ == "__main__":
+
+def main():
+
     app = QCoreApplication.instance()
     # app = QApplication.instance()
     if app is None:
         app = QApplication(sys.argv)
 
-    
     setStyle(app)
     splash_pix = QPixmap('loading.jpg')
     load_screen = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
     load_screen.setMask(splash_pix.mask())
     load_screen.show()
-
     try:
         from DeepCellDetector import DeepCellDetectorUI
         dcdui = DeepCellDetectorUI()
     except:
         e = sys.exc_info()[0]
         print( "<p>error: %s</p>" % e )
-
     load_screen.hide()
 #    sys.exit(app.exec_())
     app.exec_()
+
+
+
+if __name__ == "__main__":
+    main()
+
 
 
