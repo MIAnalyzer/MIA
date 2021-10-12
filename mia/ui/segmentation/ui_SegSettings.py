a5d5cc8e7c0c1f6faca3e3f67a962689c512d4ae
nkoerb <47297576+nkoerb@users.noreply.github.com>
2021-10-12 15:06:00 +0200

    border weighting now as user setting

--- mia/ui/segmentation/ui_SegSettings.py
+++ mia/ui/segmentation/ui_SegSettings.py
@@ -26,6 +26,18 @@ class Window(object):
         self.CBDitanceMap = QCheckBox("Separate contours",self.centralWidget)
         self.CBDitanceMap.setObjectName('nn_CreateWeightedDistanceMap')
         self.CBDitanceMap.setToolTip('Check to separate adjacent contours')
+        
+        self.SBWeight = LabelledSpinBox ('Inter countour weighting            ', self.centralWidget)
+        self.SBWeight.setObjectName('nn_SeparationWeight_seg')
+        self.SBWeight.SpinBox.setRange(2,50)
+        
+        self.SBDist = LabelledSpinBox ('Inter contour distance               ', self.centralWidget)
+        self.SBDist.setObjectName('nn_SeparationDist_seg')
+        self.SBDist.SpinBox.setRange(1,50)
+        
+        vlayout.addWidget(self.CBDitanceMap)
+        vlayout.addWidget(self.SBWeight)
+        vlayout.addWidget(self.SBDist)
 
         self.CBIgnoreBackgroundTiles = QCheckBox("Prefer labelled parts",self.centralWidget)
         self.CBIgnoreBackgroundTiles.setToolTip('When checked image parts which contain only background are ignored during training')
@@ -39,8 +51,7 @@ class Window(object):
         self.SBMinPixels.setObjectName('nn_MinLabelledPixels_seg')
         self.SBMinPixels.SpinBox.setRange(1,1000)
 
-        
-        vlayout.addWidget(self.CBDitanceMap)
+
         vlayout.addWidget(self.CBIgnoreBackgroundTiles)
         vlayout.addWidget(self.SBDiscard)
         vlayout.addWidget(self.SBMinPixels)
@@ -49,7 +60,7 @@ class Window(object):
 
         self.centralWidget.setFixedSize(vlayout.sizeHint())
         Form.setFixedSize(vlayout.sizeHint())
-                
+        
 
 
 class SegmentationSettingsWindow(QMainWindow, Window):
@@ -63,6 +74,10 @@ class SegmentationSettingsWindow(QMainWindow, Window):
         self.SBMinPixels.SpinBox.valueChanged.connect(self.MinPixelsChanged)
 
         self.CBDitanceMap.stateChanged.connect(self.ShapeSeparationChanged)
+        self.SBWeight.SpinBox.valueChanged.connect(self.WeightChanged)
+        self.SBDist.SpinBox.valueChanged.connect(self.DistChanged)
+        
+        
         
         self.CBIgnoreBackgroundTiles.setChecked(self.parent.parent.dl.augmentation.seg_ignoreBackground)
         self.CBDitanceMap.setChecked(self.parent.parent.dl.seg_useWeightedDistanceMap)
@@ -71,12 +86,24 @@ class SegmentationSettingsWindow(QMainWindow, Window):
         self.CBIgnoreBackgroundTiles.setChecked(self.parent.parent.dl.augmentation.seg_ignoreBackground)
         self.SBDiscard.SpinBox.setValue(self.parent.parent.dl.augmentation.seg_ignorecycles)
         self.SBMinPixels.SpinBox.setValue(self.parent.parent.dl.augmentation.seg_minRequiredPixels)
+        
+        self.SBWeight.SpinBox.setValue(self.parent.parent.dl.seg_border_weight)
+        self.SBDist.SpinBox.setValue(self.parent.parent.dl.seg_border_dist)
 
         self.CBDitanceMap.setChecked(self.parent.parent.dl.seg_useWeightedDistanceMap)
         super(SegmentationSettingsWindow, self).show()
         
     def ShapeSeparationChanged(self):
         self.parent.parent.dl.seg_useWeightedDistanceMap = self.CBDitanceMap.isChecked()
+        self.SBWeight.setEnabled(self.parent.parent.dl.seg_useWeightedDistanceMap)
+        self.SBDist.setEnabled(self.parent.parent.dl.seg_useWeightedDistanceMap)
+        
+        
+    def WeightChanged(self):
+        self.parent.parent.dl.seg_border_weight = self.SBWeight.SpinBox.value()
+        
+    def DistChanged(self):
+        self.parent.parent.dl.seg_border_dist = self.SBDist.SpinBox.value()
 
     def IgnoreBackgroundChanged(self):
         self.parent.parent.dl.augmentation.seg_ignoreBackground = self.CBIgnoreBackgroundTiles.isChecked()
