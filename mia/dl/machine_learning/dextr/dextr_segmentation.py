ee02ab8f6567e65dfd781d7772909d21e52fa4cb
nkoerb <47297576+nkoerb@users.noreply.github.com>
2021-12-20 15:02:51 +0100

    dextr and hed models are downloaded when used not on program start

--- mia/dl/machine_learning/dextr/dextr_segmentation.py
+++ mia/dl/machine_learning/dextr/dextr_segmentation.py
@@ -12,11 +12,13 @@ class DEXTR_Segmentation():
     def __init__(self):
         self.pad = 50
         self.thres = 0.8
-        self.modelName = get_dextr()
+        self.modelName = None 
         self.net = None
         
     def loadModel(self):
         try:
+            if self.modelName is None:
+                self.modelName =  get_dextr()
             self.net = DEXTR(nb_classes=1, resnet_layers=101, input_shape=(512, 512), weights=self.modelName, num_input_channels=4, classifier='psp', sigmoid=True)
         except:
             # can not load file
