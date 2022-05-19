fixed auto class weighting for classification

mia/dl/data/imagedata.py
@@ -306,7 +306,10 @@ class ImageData():
 
     def calcClassWeights(self):
         if self.autocalcClassWeights:
-            self.class_weights = np.sum(self.classValues)/self.classValues
-            self.class_weights /= np.sum(self.class_weights)
+            if not np.all(self.classValues):
+                self.class_weights = np.ones(self.parent.NumClasses_real, dtype = np.float)*0.5
+            else:
+                self.class_weights = np.sum(self.classValues)/self.classValues
+                self.class_weights /= np.sum(self.class_weights)
             if self.parent.NumClasses_real == 2:
                 self.class_weights = self.class_weights[1]

mia/dl/method/classification.py
@@ -174,7 +174,10 @@ class Classification(LearningMode):
         return cv2.resize(label, shape, interpolation = cv2.INTER_NEAREST)
 
     def countLabelWeight(self, label):
-        return np.zeros((self.parent.NumClasses_real), dtype = np.float)
+        weights = np.zeros((self.parent.NumClasses_real), dtype = np.float)
+        value = int(np.mean(label))
+        weights[value] = 1
+        return weights
 
     def getImageSize4ModelInput(self, inputWidth, inputHeight):
         width = self.parent.augmentation.outputwidth